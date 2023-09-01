3b44d591b613c60ce39cf521d8ad2c4f65bb9214
Vivi <sandoy@pm.me>
2023-09-01 11:25:52 +0200

    ISBN-10 assignment started

---
+++ DTE-2510/O1/ISBN-10.py
@@ -0,0 +1,24 @@
+def calc_checksum(input):
+    checksum = 0
+    for i in range(len(str(input))):
+        checksum += int(str(input)[i]) * (i+1)
+        print(str(input)[i], "*", (i+1), "=", (int(str(input)[i]) * (i+1)))
+        print(checksum)
+    if checksum % 11 == 10:
+        return "X"
+    else:
+        return checksum % 11
+
+while True:
+    try:
+        #todo: change input validation to allow for leading zeroes.
+        isbn_input = int(input("Please enter the first 9 digits of an ISBN\n"))
+        if len(str(isbn_input)) == 9:
+            break
+        #todo: add check for leading zeroes
+        else: 
+            print("That's not a valid input, please try again.")
+    except ValueError:
+        print("That's not a valid input, please try again.")
+
+print("The ISBN-10 number is", str(isbn_input) + str(calc_checksum(isbn_input)))
