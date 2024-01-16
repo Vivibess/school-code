fa865ed2e6a73cff3773b1d2477c798949e8de5a
Vivi <sandoy@pm.me>
2024-01-16 23:57:36 +0100

    open link and read file wow

--- DTE-2511/O1/baby name popularity.py
+++ DTE-2511/O1/baby name popularity.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib.request
 # 10 files for baby names called babynameranking[year].txt for years 2001-2010
 
 # each line contains a ranking, a boys name, amount of boys named that name, a girls name, amount of girls named that named
@@ -11,3 +11,24 @@ import urllib
 
 # open file from website, change url opened according to year entered
 
+
+# TODO
+# process each string in opened files to extract needed info
+# user input
+# idk
+
+
+
+
+year = "2002.txt"
+
+def open_file(year):
+    # year needs to be in the format of "yyyy.txt"
+    url = "https://liveexample.pearsoncmg.com/data/babynameranking" + year
+    file = urllib.request.urlopen(url)
+    return file
+
+
+
+for line in open_file(year):
+    print(line)
