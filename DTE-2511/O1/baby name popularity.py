790eba2a6e5e5624f5a8f3babe0c4491db0c096c
Vivi <sandoy@pm.me>
2024-01-16 23:41:50 +0100

    boop

---
+++ DTE-2511/O1/baby name popularity.py
@@ -0,0 +1,13 @@
+import urllib
+# 10 files for baby names called babynameranking[year].txt for years 2001-2010
+
+# each line contains a ranking, a boys name, amount of boys named that name, a girls name, amount of girls named that named
+
+# prompt user to enter year, gender, name, then display the ranking of the name for that year
+
+
+
+# https://liveexample.pearsoncmg.com/data/babynameranking2001.txt
+
+# open file from website, change url opened according to year entered
+
